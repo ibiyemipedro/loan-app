aa142e74d59389a85e6bc285d54a551c11651fd0
yemipedro07 <yemipedro07@gmail.com>
2021-03-20 15:33:37 +0100

    initial set-up with template

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,281 @@
+<template>
+  <div>
+    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+      <div class="row">
+        <div class="col-xl-3 col-lg-6">
+          <stats-card
+            title="Total traffic"
+            type="gradient-red"
+            sub-title="350,897"
+            icon="ni ni-active-40"
+            class="mb-4 mb-xl-0"
+          >
+            <template v-slot:footer>
+              <span class="text-success mr-2">
+                <i class="fa fa-arrow-up"></i> 3.48%
+              </span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </div>
+        <div class="col-xl-3 col-lg-6">
+          <stats-card
+            title="Total traffic"
+            type="gradient-orange"
+            sub-title="2,356"
+            icon="ni ni-chart-pie-35"
+            class="mb-4 mb-xl-0"
+          >
+            <template v-slot:footer>
+              <span class="text-success mr-2">
+                <i class="fa fa-arrow-up"></i> 12.18%
+              </span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </div>
+        <div class="col-xl-3 col-lg-6">
+          <stats-card
+            title="Sales"
+            type="gradient-green"
+            sub-title="924"
+            icon="ni ni-money-coins"
+            class="mb-4 mb-xl-0"
+          >
+            <template v-slot:footer>
+              <span class="text-danger mr-2">
+                <i class="fa fa-arrow-down"></i> 5.72%
+              </span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </div>
+        <div class="col-xl-3 col-lg-6">
+          <stats-card
+            title="Performance"
+            type="gradient-info"
+            sub-title="49,65%"
+            icon="ni ni-chart-bar-32"
+            class="mb-4 mb-xl-0"
+          >
+            <template v-slot:footer>
+              <span class="text-success mr-2">
+                <i class="fa fa-arrow-up"></i> 54.8%
+              </span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </div>
+      </div>
+    </base-header>
+
+    <div class="container-fluid mt--7">
+      <!--Charts-->
+      <div class="row">
+        <div class="col-xl-8 mb-5 mb-xl-0">
+          <card header-classes="bg-transparent">
+            <div class="row">
+              <div class="col-xs-12 col-md-6">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                eum aliquid corrupti dolorem, cupiditate quibusdam eaque.
+                Veritatis consequatur rem corporis suscipit quo, saepe autem
+                quae alias quos, doloribus reiciendis? Aliquam.
+              </div>
+              <div class="col-xs-12 col-md-6">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                eum aliquid corrupti dolorem, cupiditate quibusdam eaque.
+                Veritatis consequatur rem corporis suscipit quo, saepe autem
+                quae alias quos, doloribus reiciendis? Aliquam.
+              </div>
+            </div>
+          </card>
+        </div>
+
+        <div class="col-xl-4">
+          <card header-classes="bg-transparent">
+            <template v-slot:header>
+              <div class="row align-items-center">
+                <div class="col"></div>
+              </div>
+            </template>
+            <div class="chart-area">
+              <canvas :height="350" :id="ordersChartID"></canvas>
+            </div>
+          </card>
+        </div>
+      </div>
+      <!-- End charts-->
+    </div>
+  </div>
+</template>
+<script>
+// Charts
+import { ordersChart } from "@/components/Charts/Chart";
+import Chart from "chart.js";
+
+let chart;
+
+export default {
+  data() {
+    return {
+      salesChartID: "salesChart",
+      ordersChartID: "ordersChart",
+      bigLineChart: {
+        allData: [
+          [0, 20, 10, 30, 15, 40, 20, 60, 60],
+          [0, 20, 5, 25, 10, 30, 15, 40, 40],
+        ],
+        activeIndex: 0,
+      },
+    };
+  },
+  methods: {
+    initBigChart(index) {
+      chart.destroy();
+      chart = new Chart(
+        document.getElementById(this.salesChartID).getContext("2d"),
+        {
+          type: "line",
+          data: {
+            labels: ["May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [
+              {
+                label: "Performance",
+                tension: 0.4,
+                borderWidth: 4,
+                borderColor: "#5e72e4",
+                pointRadius: 0,
+                backgroundColor: "transparent",
+                data: this.bigLineChart.allData[index],
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+              display: false,
+            },
+            tooltips: {
+              enabled: true,
+              mode: "index",
+              intersect: false,
+            },
+            scales: {
+              yAxes: [
+                {
+                  barPercentage: 1.6,
+                  gridLines: {
+                    drawBorder: false,
+                    color: "rgba(29,140,248,0.0)",
+                    zeroLineColor: "transparent",
+                  },
+                  ticks: {
+                    padding: 0,
+                    fontColor: "#8898aa",
+                    fontSize: 13,
+                    fontFamily: "Open Sans",
+                  },
+                },
+              ],
+              xAxes: [
+                {
+                  barPercentage: 1.6,
+                  gridLines: {
+                    drawBorder: false,
+                    color: "rgba(29,140,248,0.0)",
+                    zeroLineColor: "transparent",
+                  },
+                  ticks: {
+                    padding: 10,
+                    fontColor: "#8898aa",
+                    fontSize: 13,
+                    fontFamily: "Open Sans",
+                  },
+                },
+              ],
+            },
+            layout: {
+              padding: 0,
+            },
+          },
+        }
+      );
+      this.bigLineChart.activeIndex = index;
+    },
+  },
+  mounted() {
+    chart = new Chart(
+      document.getElementById(this.salesChartID).getContext("2d"),
+      {
+        type: "line",
+        data: {
+          labels: ["May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          datasets: [
+            {
+              label: "Performance",
+              tension: 0.4,
+              borderWidth: 4,
+              borderColor: "#5e72e4",
+              pointRadius: 0,
+              backgroundColor: "transparent",
+              data: this.bigLineChart.allData[1],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          legend: {
+            display: false,
+          },
+          tooltips: {
+            enabled: true,
+            mode: "index",
+            intersect: false,
+          },
+          scales: {
+            yAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(29,140,248,0.0)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  padding: 0,
+                  fontColor: "#8898aa",
+                  fontSize: 13,
+                  fontFamily: "Open Sans",
+                },
+              },
+            ],
+            xAxes: [
+              {
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: "rgba(29,140,248,0.0)",
+                  zeroLineColor: "transparent",
+                },
+                ticks: {
+                  padding: 10,
+                  fontColor: "#8898aa",
+                  fontSize: 13,
+                  fontFamily: "Open Sans",
+                },
+              },
+            ],
+          },
+          layout: {
+            padding: 0,
+          },
+        },
+      }
+    );
+    ordersChart.createChart(this.ordersChartID);
+  },
+};
+</script>
+<style></style>
